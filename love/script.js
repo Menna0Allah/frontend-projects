86b4f6b37e6fae34f732e2565e61f2fcd481c42a
menna <menna.allah04@gmail.com>
2025-02-10 03:46:47 +0200

    some edits

--- love/script.js
+++ love/script.js
@@ -1,12 +1,17 @@
 const gheart = document.querySelector('.g-heart');
 const rheart = document.querySelector('.r-heart');
+const t = document.querySelector('.h-t');
 
 gheart.addEventListener('click', () => {
     rheart.classList.add('animation');
-    gheart.classList.add('fill-color');
+    t.style.opacity = "1";
+    setTimeout(() => {
+        rheart.classList.remove('animation');
+    t.style.opacity = "0";
+    }, 900);
 })
 
 rheart.addEventListener('click', () => {
     rheart.classList.remove('animation');
-    gheart.classList.remove('fill-color');
+    t.style.opacity = "0";
 })
